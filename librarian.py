afe3df95cabf5c085fd7bd5056db0b087809765c
Maxim Gomozov <maximgomozoff@gmail.com>
2023-04-19 22:03:13 +0300

    Add correction mode

--- librarian.py
+++ librarian.py
@@ -3,7 +3,9 @@
 import glob
 import os
 import re
+import sys
 from argparse import ArgumentParser
+from pathlib import Path
 
 import colorama
 from colorama import Fore
@@ -38,6 +40,41 @@ def colprint(text: str):
     print(colprocess(text) + Style.RESET_ALL)
 
 
+# Get file index for a given root path
+def get_index(path: str):
+    all_files = [Path(entry) for entry in glob.glob(path + '*', recursive=True) if not os.path.isdir(entry)]
+
+    index = {'follows': [], '!follows': {'possible_applies': [], '!possible_applies': []}}
+
+    colprint('Found total of |cgr||sbr|{}|rst| files in directory: |cgr||sbr|{}'.format(len(all_files), path))
+    for path in all_files:
+        filename = os.path.basename(path)
+
+        match = re.match(MOVIE_NAME_REGEX, filename)
+
+        if match is None:
+            matched = False
+            for possible_regex in POSSIBLE_MOVIE_NAME_REGEX_LIST:
+                possible_match = re.match(possible_regex, filename)
+                if possible_match is not None:
+                    name = possible_match.group('name').replace('.', ' ')
+                    year = possible_match.group('year')
+                    ext = possible_match.group('ext')
+                    possible_name = '{} ({}).{}'.format(name, year, ext)
+
+                    index['!follows']['possible_applies'].append((path, possible_name))
+
+                    matched = True
+                    break
+
+            if not matched:
+                index['!follows']['!possible_applies'].append(path)
+        else:
+            index['follows'].append(path)
+
+    return index
+
+
 # Entry point
 if __name__ == '__main__':
     # Initialization
@@ -45,7 +82,7 @@ if __name__ == '__main__':
 
     # Setup arguments
     parser = ArgumentParser(prog=PROGRAM, description=DESCRIPTION)
-    parser.add_argument('--mode', choices=['check'], required=True)
+    parser.add_argument('--mode', choices=['check', 'correct'], required=True)
     parser.add_argument('--print-follows', action='store_true', default=False)
     parser.add_argument('path')
 
@@ -56,35 +93,8 @@ if __name__ == '__main__':
 
     if mode == 'check':
         colprint('|sbr|{} ({}) |cgr|CHECK MODE'.format(PROGRAM, DESCRIPTION))
-        all_files = [entry for entry in glob.glob(path + '*', recursive=True) if not os.path.isdir(entry)]
 
-        index = {'follows': [], '!follows': {'possible_applies': [], '!possible_applies': []}}
-
-        colprint('Found total of |cgr||sbr|{}|rst| files in directory: |cgr||sbr|{}'.format(len(all_files), path))
-        for path in all_files:
-            filename = os.path.basename(path)
-
-            match = re.match(MOVIE_NAME_REGEX, filename)
-
-            if match is None:
-                matched = False
-                for possible_regex in POSSIBLE_MOVIE_NAME_REGEX_LIST:
-                    possible_match = re.match(possible_regex, filename)
-                    if possible_match is not None:
-                        name = possible_match.group('name').replace('.', ' ')
-                        year = possible_match.group('year')
-                        ext = possible_match.group('ext')
-                        possible_name = '{} ({}).{}'.format(name, year, ext)
-
-                        index['!follows']['possible_applies'].append((path, possible_name))
-
-                        matched = True
-                        break
-
-                if not matched:
-                    index['!follows']['!possible_applies'].append(path)
-            else:
-                index['follows'].append(path)
+        index = get_index(path)
 
         # Print file index
         if len(index['follows']) > 0 and args.print_follows:
@@ -108,3 +118,38 @@ if __name__ == '__main__':
             for path in index['!follows']['!possible_applies']:
                 filename = os.path.basename(path)
                 print(filename)
+
+    if mode == 'correct':
+        colprint('|sbr|{} ({}) |cgr|CORRECTION MODE'.format(PROGRAM, DESCRIPTION))
+
+        index = get_index(path)
+
+        if len(index['!follows']['possible_applies']):
+            colprint('|sbr|List of files that |crd|don\'t follow |crst|naming rules, but |cgr|can be automatically '
+                     'renamed|crst|:')
+            for path, suggestion in index['!follows']['possible_applies']:
+                filename = os.path.basename(path)
+                colprint('|crd|{}|crst| -> |cgr|{}'.format(filename, suggestion))
+            print()
+
+            proceed = None
+            while proceed is None:
+                ans = input('Do you wish to proceed? (Y/N): ')
+
+                if ans == 'Y':
+                    proceed = True
+                if ans == 'N':
+                    proceed = False
+
+            if not proceed:
+                colprint('|sbr||crd|Abort.')
+                sys.exit(1)
+
+            for path, suggestion in index['!follows']['possible_applies']:
+                filename = os.path.basename(path)
+                colprint('Renaming |crd|{}|crst| -> |cgr|{}'.format(filename, suggestion))
+                # print('{} -> {}'.format(path, path.parent / suggestion))
+                os.rename(path, path.parent / suggestion)
+
+        else:
+            colprint('|sbr||crd|There are no files that can be automatically corrected.')
